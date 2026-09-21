4d10ec43b28168ff545113870565bbef87803fd4
Gift Sape <giftsape@gmail.com>
2026-09-21 22:34:07 +0200

    feature: redisterd DbContext and configure SQL Server Connection String

--- RaceDay.Api/Program.cs
+++ RaceDay.Api/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using RaceDay.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<RaceDayDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RaceDayDb")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
